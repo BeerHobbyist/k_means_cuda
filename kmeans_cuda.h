612a60e0223db7d70383ccb1fb1a0ad5d9636484
Ignacy Walużenicz <128468778+BeerHobbyist@users.noreply.github.com>
2025-12-03 19:58:33 +0100

    Remove unused header

--- kmeans_cuda.h
+++ kmeans_cuda.h
@@ -5,10 +5,3 @@ void run_atomic_kmeans(const std::vector<float>& h_points,
                        int N, int D, int K,
                        float threshold, int maxIters,
                        std::vector<float>& h_centroids);
-
-void run_reduce_kmeans(const std::vector<float>& h_points,
-                       int N, int D, int K,
-                       float threshold, int maxIters,
-                       std::vector<float>& h_assignments);
-
-
